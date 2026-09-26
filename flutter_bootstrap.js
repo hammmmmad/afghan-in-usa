680c35b90c96413148246e1cfbd60dba6e7188e2
Hamid jan <hammmmmad@users.noreply.github.com>
2026-09-26 00:44:07 -0700

    Deploy web: IR & AF in USA rename, Vazirmatn Persian font, nav bar day/night styling

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "507988396" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3935897015" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
